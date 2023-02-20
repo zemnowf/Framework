4987ad1a6c5fc61bdf6253f58388a7394e11427a
Daniil Zemnov <zemnowf@gmail.com>
2023-02-20 21:59:53 +0300

    add configs

--- Fw/Core/Config.php
+++ Fw/Core/Config.php
@@ -4,5 +4,29 @@ namespace Fw\Core;
 
 class Config
 {
+    private static array $configs = [];
 
+    public function __construct()
+    {
+    }
+
+    public static function get(string $path)
+    {
+        require_once 'Fw/config.php';
+
+        $keys = explode('/', $path);
+        $config = self::$configs;
+
+        foreach ($keys as $key) {
+            $config = $config[$key];
+        }
+
+        return $config;
+
+    }
+
+    public static function setConfig($config, $data, $variable)
+    {
+        self::$configs[$config][$data] = $variable;
+    }
 }
--- index.php
+++ index.php
@@ -4,8 +4,12 @@ require './Fw/init.php';
 
 use Fw\Core\Application;
 use Fw\Core\Route;
+use Fw\Core\Config;
 
 Route::route();
 
 $action = $_SERVER['REQUEST_URI'];
 Route::dispatch($action);
+
+//test
+echo Config::get("db/login");
