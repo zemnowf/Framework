14eed0cc6478890b7019169264cdfe6292f61f45
Daniil Zemnov <zemnowf@gmail.com>
2023-02-22 12:56:21 +0300

    add buffer handling

--- Fw/Core/Application.php
+++ Fw/Core/Application.php
@@ -4,14 +4,57 @@ namespace Fw\Core;
 
 class Application
 {
+    const TEMPLATES = '../templates';
+    const TEMPLATE = 'template/id';
+    const HEADER = '/header.php';
+    const FOOTER = '/footer.php';
+
     private $pager = null;
-    private static $instance = null;
     private $template = null;
+    private $buffer = false;
 
     public function __construct()
     {
         Route::route();
         Config::configure();
+        $this->header();
+    }
+
+    public function header()
+    {
+        $this->startBuffer();
+
+        //fix
+        include self::TEMPLATES . Config::get(self::TEMPLATE) . self::HEADER;
+    }
+
+    public function footer()
+    {
+        include self::TEMPLATES . Config::get(self::TEMPLATE) . self::FOOTER;
+        echo $this->endBuffer();
+    }
+
+    public function startBuffer()
+    {
+        ob_start();
+        $this->buffer = true;
+    }
+
+    public function restartBuffer()
+    {
+        if ($this->buffer) {
+            ob_clean();
+        }
+        $this->startBuffer();
+    }
+
+    public function endBuffer()
+    {
+        $content = ob_get_clean();
+        $this->buffer = false;
+
+        //fix
+        return $content;
     }
 
 }
