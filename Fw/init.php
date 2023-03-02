92500edbe3dde38c9d84fcf6346d45eadc25051e
Daniil Zemnov <zemnowf@gmail.com>
2023-03-02 15:30:48 +0300

    fix init

--- Fw/init.php
+++ Fw/init.php
@@ -3,6 +3,7 @@ session_start();
 
 const FW_CORE_CONNECTION = true;
 const TEMPLATES = __DIR__ . '/templates/';
+const COMPONENTS_ROOT = __DIR__ . "/components/";
 
 spl_autoload_register(function (string $class) {
 
