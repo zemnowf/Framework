0a8ae99b9d9f5a47d2d072615c10bc5fcd5abb48
Daniil Zemnov <zemnowf@gmail.com>
2023-03-02 15:32:14 +0300

    fix header

--- Fw/templates/main/header.php
+++ Fw/templates/main/header.php
@@ -11,16 +11,21 @@ Route::dispatch($action);
 ?>
 
 
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <? $application->getPage()->showHead(); ?>
-    <title><?= $application->getPage()->showProperty('title'); ?>></title>
-</head>
-<body>
-<header><p>Header</p></header>
-<main>
+    <!doctype html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport"
+              content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <? $application->getPage()->showHead(); ?>
+        <title><? $application->getPage()->showProperty('title'); ?>></title>
+    </head>
+    <body>
+    <header><p>Header</p></header>
+    <main>
+
+<?php
+$action = $_SERVER['REQUEST_URI'];
+Route::dispatch($action);
+
